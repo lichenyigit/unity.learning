0e366392d41ad28688f79c9217c8e486ee5ace01
lichenyi <lichenyiwin@outlook.com>
2018-12-11 11:12:10 +0800

    完善

---
+++ Assets/_Drag/Scripts/DragController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragController : MonoBehaviour, IEndDragHandler, IBeginDragHandler, IDragHandler
+{
+    public void OnEndDrag(PointerEventData eventData)
+    {
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+    }
+}
---
+++ Assets/_WWWReload/Scripts/AddBoxColidderController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddBoxColidderController : MonoBehaviour {
+
+	void Start ()
+	{
+		int count = gameObject.transform.childCount;
+		for (int i = 0;i < count;i++)
+		{
+			gameObject.transform.GetChild(i).gameObject.AddComponent<BoxCollider>();
+		}
+	}
+
+}
