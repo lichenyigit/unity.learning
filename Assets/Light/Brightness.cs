893133b15fb8ad37cbf9d8efbada7704d2a62e08
lichenyi <lichenyiwin@outlook.com>
2019-12-30 11:17:05 +0800

    perfetch

---
+++ Assets/Light/Brightness.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Brightness : MonoBehaviour
+{
+    public Scrollbar scrollbar;
+
+    private void Start()
+    {
+        scrollbar.onValueChanged.AddListener(SetApplicationBrightnessTo);
+    }
+
+    private void SetApplicationBrightnessTo(float Brightness)
+    {
+        AndroidJavaObject Activity = null;
+        Activity = new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity");
+        Activity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+        {
+            AndroidJavaObject Window = null, Attributes = null;
+            Window = Activity.Call<AndroidJavaObject>("getWindow");
+            Attributes = Window.Call<AndroidJavaObject>("getAttributes");
+            Attributes.Set("screenBrightness", Brightness);
+            Window.Call("setAttributes", Attributes);
+        }));
+    }
+
+    
+}
---
+++ Assets/Quality/QualityController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QualityController : MonoBehaviour
+{
+    public void SetQuality(int index)
+    {
+        /*for (int i = 0; i < QualitySettings.names.Length; i++)
+        {
+            Debug.Log(QualitySettings.names[i].ToString());
+        }*/
+        QualitySettings.SetQualityLevel(index, true);
+        Debug.Log(QualitySettings.names[index].ToString());
+    }
+}
